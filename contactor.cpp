27f92b7acc6b1ceeb8703281700874a7f1f8808e
MarttiMeri <martti.meri@gmail.com>
2015-11-16 15:29:54 +0200

    Create contactor.cpp

---
+++ contactor.cpp
@@ -0,0 +1,12 @@
+
+// main program
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "This is contactor";
+  cout << "Wellcome";
+  cout << "How to utilize GitHub, testing pulling and pushing with this"
+}
